ebd25e6310215dfdae6a7fb278c1414504f0bf22
Maxim <117824059+selfdestroying@users.noreply.github.com>
2024-05-19 15:20:25 +0300

    Update database.js

--- database.js
+++ database.js
@@ -1,12 +1,14 @@
-const users = [
+export const users = [
     {
         id: 1,
         name: 'John',
-        password: '123456'
+        username: 'john',
+        password: '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92'
     },
     {
         id: 2,
         name: 'Jane',
-        password: 'abcdef'
+        username: 'jane',
+        password: '65e84be33532fb784c48129675f9eff3a682b27168c0ea744b2cf58ee02337c5'
     }
 ]
